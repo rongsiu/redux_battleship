ed043c8ce516975e0a714d4012909478da2ffc60
Rongsiu Lim <rongsiu@gmail.com>
2018-08-26 14:21:13 -0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/src');
+const DIST_DIR = path.join(__dirname, '/dist');
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
+    devtool: 'source-map',
+};
